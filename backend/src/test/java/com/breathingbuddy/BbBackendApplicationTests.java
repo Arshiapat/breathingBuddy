017a4c1fa8becaef38df2fb1ab41eb285f90418c
Arshiapat <patel.arshia22@gmail.com>
2025-05-10 21:55:06 -0400

    Initialized Flutter frontend

---
+++ backend/src/test/java/com/breathingbuddy/BbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breathingbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
